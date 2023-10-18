a76e7d46c0f0f06515899d9b24b4dd80f51eed7b
Siamak Heshmati <siamakheshmati@gmail.com>
2023-10-18 16:15:46 -0700

    variable names - updated across the codebase

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -2,44 +2,41 @@ import * as cdk from "aws-cdk-lib";
 import { Stack } from "aws-cdk-lib";
 import { ImagebuilderPipeline } from './../lib/imagebuilderpipeline'
 import { MainConfig } from './../lib/interface/mainConfig'
-import * as ec2 from "aws-cdk-lib/aws-ec2";
-import * as s3 from "aws-cdk-lib/aws-s3";
-import * as sns from "aws-cdk-lib/aws-sns";
+
+// The following imports are needed when you set the props
+//import * as ec2 from "aws-cdk-lib/aws-ec2";
+//import * as s3 from "aws-cdk-lib/aws-s3";
+//import * as sns from "aws-cdk-lib/aws-sns";
 
 const app = new cdk.App();
+// You can use this variable for your props or you can directly pass them to the class. There are two examples in the "example_props" folder
+let amiConfig: MainConfig
 
-// const ami_config: MainConfing = user_config;
+const tagEnv = new cdk.Tag('env', 'dev');
+const tagName = new cdk.Tag('Name', 'golden-ami-blog-demo');
 
-// const tag: cdk.Tag = {
-//   "env": "dev",
-//   "Name": "golden-ami-blog-demo"
-// }
-const tag_env = new cdk.Tag('env', 'dev');
-const tag_name = new cdk.Tag('Name', 'golden-ami-blog-demo');
-
-export class createImageBuilder extends cdk.Stack {
+export class CreateImageBuilder extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
     // Props can be passed using ami_config variable. For example, refer to the folder `example_props`
-    let ami_config: MainConfig 
    
-    const Golden_AMI_Pipeline = new ImagebuilderPipeline(this, "ImagebuilderPipeline", {
-      user_config: ami_config!
+    const GOLDEN_AMI_PIPELINE = new ImagebuilderPipeline(this, "ImagebuilderPipeline", {
+      userConfig: amiConfig!
     });
-    new cdk.CfnOutput(this, 'S3bucketName', { value: Golden_AMI_Pipeline.bucket.bucketName });
-    new cdk.CfnOutput(this, 'PipelineName', { value: Golden_AMI_Pipeline.pipeline.name });
-    new cdk.CfnOutput(this, 'ImageRecipeName', { value: Golden_AMI_Pipeline.recipe.name });
-    new cdk.CfnOutput(this, 'ImagePipelineInfraName', { value: Golden_AMI_Pipeline.infra.name });
-    new cdk.CfnOutput(this, 'AMIInstanceProfile', { value: Golden_AMI_Pipeline.instance_profile_role.instanceProfileName! });
+    new cdk.CfnOutput(this, 'S3bucketName', { value: GOLDEN_AMI_PIPELINE.bucket.bucketName });
+    new cdk.CfnOutput(this, 'PipelineName', { value: GOLDEN_AMI_PIPELINE.pipeline.name });
+    new cdk.CfnOutput(this, 'ImageRecipeName', { value: GOLDEN_AMI_PIPELINE.recipe.name });
+    new cdk.CfnOutput(this, 'ImagePipelineInfraName', { value: GOLDEN_AMI_PIPELINE.infra.name });
+    new cdk.CfnOutput(this, 'AMIInstanceProfile', { value: GOLDEN_AMI_PIPELINE.instanceProfileRole.instanceProfileName! });
 
   }
 }
-const stack = new createImageBuilder(app, "ImagebuilderPipeline", {
+const stack = new CreateImageBuilder(app, "ImagebuilderPipeline", {
   env: {
     region: process.env.CDK_DEPLOY_REGION
   }
 }
 )
-cdk.Tags.of(stack).add(tag_env.key,tag_env.value)
-cdk.Tags.of(stack).add(tag_name.key,tag_name.value)
+cdk.Tags.of(stack).add(tagEnv.key,tagEnv.value)
+cdk.Tags.of(stack).add(tagName.key,tagName.value)
 app.synth();
--- lib/interface/mainConfig.ts
+++ lib/interface/mainConfig.ts
@@ -1,37 +1,37 @@
-import { ComponentConfig } from "./component_config";
-import { distribution } from "./distribution";
-import { infrastructure } from "./Infrastructure";
-import { Recipe } from "./Recipe";
+import { ComponentConfig } from "./componentConfig";
+import { Distribution } from "./distribution";
+import { Infrastructure } from "./infrastructure";
+import { Recipe } from "./recipe";
 import * as sns from "aws-cdk-lib/aws-sns";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as cdk from "aws-cdk-lib";
-import { Tags } from "./tag_interfaces";
+import { Tags } from "./tagInterfaces";
 
 export interface MainConfig {
   baseImage: ec2.IMachineImage
-  ami_component_bucket_name?: s3.IBucket;
-  ami_component_bucket_create?: boolean;
-  ami_component_bucket_version?: boolean;
+  amiComponentBucketName?: s3.IBucket;
+  amiComponentBucketCreate?: boolean;
+  amiComponentBucketVersion?: boolean;
   imagePipelineName?: string;
   instanceProfileName?: string;
   instanceProfileRoleName?: string;
   iamEncryption?: boolean;
-  components_prefix: string;
-  key_alias?: string;
-  image_recipe: Recipe;
-  sns_topic?: sns.ITopic
-  attr?: string;
+  componentsPrefix: string;
+  keyAlias?: string;
+  imageRecipe: Recipe;
+  snsTopic?: sns.ITopic
+  attribute?: string;
   amitag?: Tags;
   tag?: Tags;
   schedule?: object;
-  infrastructure?: infrastructure;
-  Component_Config: ComponentConfig;
-  Distribution?: distribution[];
+  infrastructure?: Infrastructure;
+  componentConfig: ComponentConfig;
+  distributionConfig?: Distribution[];
   distributionName?: string;
   distributionDescription?: string;
-  resource_removal_policy?: cdk.RemovalPolicy;
-  default_Component_Config?: ComponentConfig
+  resourceRemovalPolicy?: cdk.RemovalPolicy;
+  defaultComponentConfig?: ComponentConfig
 }
 
 
